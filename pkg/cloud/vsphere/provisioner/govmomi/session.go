3d15af11301641b7e3dda7da782f90a497c9b0e5
thorsteinnth <thorsteinnth@gmail.com>
2019-07-02 18:49:32 +0000

    cache rest clients

--- pkg/cloud/vsphere/provisioner/govmomi/session.go
+++ pkg/cloud/vsphere/provisioner/govmomi/session.go
@@ -6,8 +6,10 @@ import (
 	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/find"
 	"github.com/vmware/govmomi/vapi/rest"
+	"github.com/vmware/govmomi/vapi/tags"
 	"github.com/vmware/govmomi/vim25"
 	"github.com/vmware/govmomi/vim25/soap"
+	"k8s.io/klog"
 	"net/url"
 	vsphereutils "sigs.k8s.io/cluster-api-provider-vsphere/pkg/cloud/vsphere/utils"
 	clusterv1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
@@ -17,6 +19,11 @@ type SessionContext struct {
 	session *govmomi.Client
 	context *context.Context
 	finder  *find.Finder
+}
+
+// NetApp
+type RestSessionContext struct {
+	restClient *rest.Client
 }
 
 func (pv *Provisioner) sessionFromProviderConfig(cluster *clusterv1.Cluster, machine *clusterv1.Machine) (*SessionContext, error) {
@@ -62,6 +69,7 @@ func (pv *Provisioner) sessionFromProviderConfig(cluster *clusterv1.Cluster, mac
 // NetApp
 func (pv *Provisioner) restClientFromProviderConfig(cluster *clusterv1.Cluster) (*rest.Client, error) {
 
+	var rsc RestSessionContext
 	ctx := context.Background()
 
 	vsphereConfig, err := vsphereutils.GetClusterProviderSpec(cluster.Spec.ProviderSpec)
@@ -69,14 +77,24 @@ func (pv *Provisioner) restClientFromProviderConfig(cluster *clusterv1.Cluster) 
 		return nil, err
 	}
 
-	soapURL, err := soap.ParseURL(vsphereConfig.VsphereServer)
-	if soapURL == nil || err != nil {
-		return nil, fmt.Errorf("error parsing vSphere URL %s : [%s]", soapURL, err)
-	}
-
 	username, password, err := pv.GetVsphereCredentials(cluster)
 	if err != nil {
 		return nil, err
+	}
+
+	if cachedClientContext, ok := pv.restsessioncache[vsphereConfig.VsphereServer+username]; ok {
+		cachedClientContext, ok := cachedClientContext.(RestSessionContext)
+		if ok {
+			if ok := restClientActive(cachedClientContext.restClient); ok {
+				klog.V(4).Infof("using cached rest client for server %s and user %s", vsphereConfig.VsphereServer, username)
+				return cachedClientContext.restClient, nil
+			}
+		}
+	}
+
+	soapURL, err := soap.ParseURL(vsphereConfig.VsphereServer)
+	if soapURL == nil || err != nil {
+		return nil, fmt.Errorf("error parsing vSphere URL %s : [%s]", soapURL, err)
 	}
 
 	soapClient := soap.NewClient(soapURL, true)
@@ -85,10 +103,30 @@ func (pv *Provisioner) restClientFromProviderConfig(cluster *clusterv1.Cluster) 
 		return nil, err
 	}
 
+	klog.V(4).Infof("creating new rest client for server %s and user %s", vsphereConfig.VsphereServer, username)
 	restClient := rest.NewClient(vimClient)
 	if err := restClient.Login(ctx, url.UserPassword(username, password)); err != nil {
 		return nil, err
 	}
 
+	rsc.restClient = restClient
+
+	pv.restsessioncache[vsphereConfig.VsphereServer+username] = rsc
+
 	return restClient, nil
+}
+
+// NetApp
+func restClientActive(client *rest.Client) bool {
+
+	// NOTE: Rest client does not expose an IsActive check out of the box. Rolling our own rudimentary one.
+
+	tm := tags.NewManager(client)
+
+	_, err := tm.GetTags(context.TODO())
+	if err != nil {
+		return false
+	}
+
+	return true
 }
